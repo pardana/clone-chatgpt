001b844fe95f89b85107824baed5aef4e0f9a76c
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-25 16:15:57 +0700

    update api

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,20 +20,40 @@ export default function Home() {
       { type: "user", message: inputValue },
     ]);
 
+    sendMessage(inputValue);
+
     setInputValue("");
   };
 
   const sendMessage = (message) => {
     const url = "https://api.openai.com/v1/chat/completions";
-    
+
     const headers = {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
     };
 
     const data = {
-      model: ""
-    }
+      model: "gpt-3.5-turbo",
+      message: [{ role: "user", content: message }],
+    };
+
+    setIsLoading(true);
+
+    axios
+      .post(url, data, { headers: headers })
+      .then((response) => {
+        console.log(response);
+        setChatLog((prevChatLog) => [
+          ...prevChatLog,
+          { type: "bot", message: response.data.choices[0].message.content },
+        ]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
   };
 
   return (
